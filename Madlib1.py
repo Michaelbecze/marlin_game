b071953c30bad648fffd8cc4846806893c62f10d
michael Becze <michael.becze@gmail.com>
2023-07-21 07:12:59 -0400

    1st revsion

---
+++ Madlib1.py
@@ -0,0 +1,9 @@
+noun1 = input("Noun...")
+verb1 = input("Verb...")
+noun2 = input("Noun...")
+adjective1 = input("Adjective...")
+
+story = "One day there was a " + (noun1) + " who liked to go " + (verb1) + \
+". " "On the way to the grocery store they saw a " + (noun2) + " and it was very " + (adjective1) + "."
+
+print(story)
